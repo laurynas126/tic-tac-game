7ec7e73aef4846ae726e30d7ce6538038b32f3fb
Laurynas126 <laurynas126@gmail.com>
2017-10-04 14:42:11 +0300

    Change error handling

--- Parser.hs
+++ Parser.hs
@@ -1,13 +1,21 @@
 module Parser where
 
+import Data.Either
+
 data MoveData = MoveData {
     moveC :: (Int, Int),
     moveID :: String,
     moveV :: Char
-} deriving Show
+} deriving (Show, Eq)
 
 test :: MoveData
 test = MoveData (0,1) "abc" '4'
+
+nullCoordinates :: (Int, Int)
+nullCoordinates = (-1,-1)
+
+newMove :: MoveData
+newMove = MoveData nullCoordinates "" '\\'
 
 empty :: String
 empty = "de"
@@ -22,7 +30,7 @@ msg3 :: String
 msg3 = "d2:id5:MY_ID1:cli1ei2eee"
 
 msg4 :: String
-msg4 = "d2:id5:MY_ID1:cli1ei2ee1:v1:o4:prevd2:id5:UR_ID1:cli2ei0ee1:v1:ze"
+msg4 = "d2:id5:MY_ID1:cli1ei2ee1:v1:o4:prevd2:id5:UR_ID1:cli2ei0ee1:v1:zee"
 
 msg5 :: String
 msg5 = "d2:id5:MY_ID1:cli1ei2ee1:v1:o4:prevd2:id5:UR_ID1:cli2ei0ee1:v1:z4:prevd2:id5:UR_ID1:cli1ei1ee1:v1:Geee"
@@ -40,8 +48,16 @@ message3 = "d1:cli0ei2ee2:id1:w4:prevd1:cli2ei1ee2:id5:WgEYE4:prevd1:cli1ei1ee2:
 message4 :: String
 message4 = "d1:cli2ei0ee2:id25:MnQxKaeMJPTGsApXOrSwiSNnX4:prevd1:cli1ei2ee2:id22:MCWPFWuHBPApBdNBKxjbud4:prevd1:cli2ei2ee2:id25:MnQxKaeMJPTGsApXOrSwiSNnX4:prevd1:cli2ei1ee2:id22:MCWPFWuHBPApBdNBKxjbud4:prevd1:cli0ei0ee2:id25:MnQxKaeMJPTGsApXOrSwiSNnX4:prevd1:cli0ei2ee2:id22:MCWPFWuHBPApBdNBKxjbud4:prevd1:cli1ei0ee2:id25:MnQxKaeMJPTGsApXOrSwiSNnX4:prevd1:cli0ei1ee2:id22:MCWPFWuHBPApBdNBKxjbud1:v1:oe1:v1:oe1:v1:oe1:v1:oe1:v1:oe1:v1:xe1:v1:oe1:v1:xe"
 
+fromLeft :: a -> Either a b -> a
+fromLeft _ (Left a) = a
+fromLeft a _        = a
+
+fromRight :: b -> Either a b -> b
+fromRight _ (Right b) = b
+fromRight b _        = b
 
 parseInt :: String -> (Int, String)
+parseInt [] = (-1,[])
 parseInt (num:rest) 
     | num == 'i' =
         let
@@ -49,19 +65,40 @@ parseInt (num:rest)
             strLength = length iAsStr + 1
             rest1 = drop strLength rest
             in (read iAsStr, rest1)
-    | otherwise = error "No Integer"
-parseInt [] = error "Empty list"
+    | otherwise = (-1,rest)
 
-parseDict :: String -> ([MoveData], String)
-parseDict "de" = ([], "")
-parseDict ('d':t) = parseDict' t [MoveData (-1,-1) "" '\\'] 0
+move :: String -> Either String (Maybe (Int, Int, Char))
+move x = 
+    let 
+        validateResult = validateGame (parseDict x)
+        value 
+            | isLeft validateResult = Left $ fromLeft  "Could not validate" validateResult
+            | otherwise = Right (nextMove (fromRight  [] validateResult))
+        in value
+
+validateGame :: Either String [MoveData] -> Either String [MoveData]
+validateGame (Left a) = Left a
+validateGame (Right []) = Right []
+validateGame (Right b) = Right b
+
+nextMove :: [MoveData] -> Maybe (Int, Int, Char)
+nextMove [] = Just (1,1,'x')
+nextMove _ = Nothing
+
+parseDict :: String -> Either String [MoveData]
+parseDict "de" = Right []
+parseDict ('d':t) = parseDict' t [newMove] 0
     where
-        parseDict' :: String -> [MoveData] -> Int -> ([MoveData], String)
-        parseDict' [] acc _ = (acc, "")
-        parseDict' symbol [] _ = ([], symbol)
-        parseDict' (symbol:rest) moveList@(_:acc) depth
-            | symbol == 'd' || symbol == 'l' = parseDict' rest moveList (depth)
+        parseDict' :: String -> [MoveData] -> Int -> Either String [MoveData]
+        parseDict' [] acc _
+            | null acc = Right []
+            | head acc == newMove = Right []
+            | otherwise = Right acc
+        parseDict' (symbol:rest) moveList depth
+            | symbol == 'd' || symbol == 'l' = parseDict' rest moveList depth
             | symbol == 'e' = parseDict' rest moveList (depth + 1)
+            | symbol `elem` ['0'..'9'] && notElem ':' rest 
+                = Left "Invalid data format: number not terminated with ':'"
             | symbol `elem` ['0'..'9'] =
             let
                 (fstPart, sndPart) = splitAt depth moveList
@@ -70,21 +107,17 @@ parseDict ('d':t) = parseDict' t [MoveData (-1,-1) "" '\\'] 0
                 (move2, remainder2)
                     | value == "id" = parseID move remainder
                     | value == "c" = parseCoordinates move remainder
-                    | value == "prev" = (MoveData (-1,-1) "" '\\':[move], remainder)
+                    | value == "prev" = (newMove:[move], remainder)
                     | value == "v" = parseV move remainder
-                    | otherwise = ([move], remainder)
-                depth2
-                    | value == "prev" = depth
-                    | otherwise = depth
-
-                -- in parseDict' remainder2 (move2 ++ acc) depth2
-                in parseDict' remainder2 (fstPart ++ move2 ++ tail sndPart) depth2
-            | otherwise = (moveList , rest)
-parseDict _ = error "List expected"
+                    | otherwise = ([move], value)
+                in parseDict' remainder2 (fstPart ++ move2 ++ tail sndPart) depth
+            | otherwise = Left ("Invalid data format: symbol " ++ [symbol] ++ " found")
+parseDict _ = Left "Invalid data: Dictionary expected"
 
 parseValue :: String -> (String, String)
 parseValue [] = ([], [])
-parseValue str =
+parseValue str@(first:xs)
+    | first `elem` ['0'..'9'] && ':' `elem` str && null [x | x <- takeWhile(/= ':') str, x `notElem` ['0'..'9']] =
     let
         iAsStr = takeWhile(/= ':') str
         strLength = length iAsStr + 1
@@ -92,6 +125,7 @@ parseValue str =
         value = take number $ drop strLength str
         remainder = drop (strLength + number) str
     in (value, remainder)
+    | otherwise = ([first], xs)
 
 parseID :: MoveData -> String -> ([MoveData], String)
 parseID move [] = ([move], [])
@@ -107,7 +141,11 @@ parseCoordinates m ('l':rest) =
     let 
         (x, rem1) = parseInt rest
         (y, rem2) = parseInt rem1
-        in ([m {moveC = (x, y)}], drop 1 rem2)
+        terminator = take 1 rem2
+        rem3 
+            | terminator == "e" = drop 1 rem2
+            | otherwise = rem2
+        in ([m {moveC = (x, y)}], rem3)
 parseCoordinates _ _ = error "Incorrect coordinate format"
 
 parseV :: MoveData -> String -> ([MoveData], String)
