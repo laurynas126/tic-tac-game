f66b17315ab8535da957a6bd6933a7834dde6fa4
Laurynas126 <laurynas126@gmail.com>
2017-10-10 11:59:50 +0300

    Fixed error handling; Refactorings

--- Parser.hs
+++ Parser.hs
@@ -10,7 +10,7 @@ data MoveData = MoveData {
 } deriving (Show, Eq)
 
 legalMoves :: [(Int, Int)]
-legalMoves = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+legalMoves = [(1,1),(0,0),(0,1),(0,2),(1,0),(1,2),(2,0),(2,1),(2,2)]
 
 test :: MoveData
 test = MoveData (0,1) "abc" '4'
@@ -21,8 +21,8 @@ nullCoordinates = (-1,-1)
 newMove :: MoveData
 newMove = MoveData nullCoordinates "" '\\'
 
-msg :: String
-msg = "di42ei0ei777ee"
+msg1 :: String
+msg1 = "di42ei0ei777ee"
 
 msg2 :: String
 msg2 = "d2:idi16ee"
@@ -54,23 +54,23 @@ fromLeft a _        = a
 
 fromRight :: b -> Either a b -> b
 fromRight _ (Right b) = b
-fromRight b _        = b
+fromRight b _         = b
 
 parseInt :: String -> (Int, String)
 parseInt [] = (-1,[])
 parseInt (num:rest) 
-    | num == 'i' =
+    | num == 'i' && 'e' `elem` rest && null [x | x <- takeWhile(/= 'e') rest, x `notElem` ['0'..'9']] =
         let
             iAsStr = takeWhile(/= 'e') rest
             strLength = length iAsStr + 1
             rest1 = drop strLength rest
             in (read iAsStr, rest1)
-    | otherwise = (-1,rest)
+    | otherwise = (-99,rest)
 
 move :: String -> Either String (Maybe (Int, Int, Char))
-move x = 
+move msg = 
     let 
-        validateResult = validateGame (parseDict x)
+        validateResult = validateGame (parseDict msg)
         value 
             | isLeft validateResult = Left $ fromLeft  "Could not validate" validateResult
             | otherwise = Right (nextMove (fromRight  [] validateResult))
@@ -112,22 +112,26 @@ parseDict ('d':t) = parseDict' t [newMove] 0
             | head acc == newMove = Right []
             | otherwise = Right acc
         parseDict' (symbol:rest) moveList depth
+            | depth == -99 = Left "Invalid data format: unexpected termination of dictionary/list: "
             | symbol == 'd' || symbol == 'l' = parseDict' rest moveList depth
             | symbol == 'e' = parseDict' rest moveList (depth + 1)
             | symbol `elem` ['0'..'9'] && notElem ':' rest 
                 = Left "Invalid data format: number not terminated with ':'"
             | symbol `elem` ['0'..'9'] =
             let
-                (fstPart, sndPart) = splitAt depth moveList
-                move = moveList !! depth
+                index
+                    | length moveList > depth = depth
+                    | otherwise = -99
+                (fstPart, sndPart) = splitAt index moveList
+                move1 = moveList !! index
                 (value, remainder) = parseValue (symbol:rest)
                 (move2, remainder2)
-                    | value == "id" = parseID move remainder
-                    | value == "c" = parseCoordinates move remainder
-                    | value == "prev" = (newMove:[move], remainder)
-                    | value == "v" = parseV move remainder
-                    | otherwise = ([move], value)
-                in parseDict' remainder2 (fstPart ++ move2 ++ tail sndPart) depth
+                    | value == "id" = parseID move1 remainder
+                    | value == "c" = parseCoordinates move1 remainder
+                    | value == "prev" = (newMove:[move1], remainder)
+                    | value == "v" = parseV move1 remainder
+                    | otherwise = ([move1], value)
+                in parseDict' remainder2 (fstPart ++ move2 ++ tail sndPart) index
             | otherwise = Left ("Invalid data format: symbol " ++ [symbol] ++ " found")
 parseDict _ = Left "Invalid data: Dictionary expected"
 
@@ -145,15 +149,15 @@ parseValue str@(first:xs)
     | otherwise = ([first], xs)
 
 parseID :: MoveData -> String -> ([MoveData], String)
-parseID move [] = ([move], [])
-parseID move str =
+parseID move1 [] = ([move1], [])
+parseID move1 str =
     let
         (value, remainder) = parseValue str
-    in ([move {moveID = value}], remainder)
+    in ([move1 {moveID = value}], remainder)
 
 parseCoordinates :: MoveData -> String -> ([MoveData], String)
 parseCoordinates m [] = ([m], [])
-parseCoordinates m "le" = ([m], [])
+parseCoordinates m ('l':'e':rest) = ([m], rest)
 parseCoordinates m ('l':rest) = 
     let 
         (x, rem1) = parseInt rest
@@ -163,11 +167,15 @@ parseCoordinates m ('l':rest) =
             | terminator == "e" = drop 1 rem2
             | otherwise = rem2
         in ([m {moveC = (x, y)}], rem3)
-parseCoordinates _ _ = error "Incorrect coordinate format"
+parseCoordinates m str = ([m], str)
 
 parseV :: MoveData -> String -> ([MoveData], String)
-parseV move [] = ([move], [])
-parseV move str =
+parseV move1 [] = ([move1], [])
+parseV move1 str =
     let
         (value, remainder) = parseValue str
-    in ([move {moveV = head value}], remainder)
+        moveSymbol
+            | null value = '\\'
+            | length value > 1 = '\\'
+            | otherwise = head value
+    in ([move1 {moveV = moveSymbol}], remainder)
