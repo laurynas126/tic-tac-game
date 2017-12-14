6765106b3f619c55e6441b6b9927b956ac662cbc
Laurynas126 <laurynas126@gmail.com>
2017-12-15 00:23:31 +0200

    Update history page response; Delete unused functions and comments

--- tic-tac-server/src/HttpServer.hs
+++ tic-tac-server/src/HttpServer.hs
@@ -3,6 +3,7 @@ module HttpServer where
 
 import Data.List
 import qualified Data.Text as T
+import Data.Text.Encoding
 import Data.String.Conversions
 import Network.Wai
 import Network.Wai.Handler.Warp
@@ -25,29 +26,32 @@ runServer = do
     tvar <- newTVarIO str
     run port (app tvar)
     
--- app req respond = respond $
---     case pathInfo req of
---         ["history"] -> history
---         x -> postResponse req
- 
 app tvar req respond = do
     gameIDs <- atomically $ readTVar tvar
     body <- requestBody req
+    let reqMethod = requestMethod req
     let response = case pathInfo req of
             ["history"] -> history gameIDs
-            [x] -> if requestMethod req == methodPost then
+            [_] -> if reqMethod == methodPost then
                     postResponse body
                  else invalidRequest
             _ -> invalidRequest
     let game = head (pathInfo req)
-    if length (pathInfo req) == 1 && game /= "history" && game `notElem` gameIDs then do
-        a <- atomically $ writeTVar tvar ((head (pathInfo req)):gameIDs)
+    if reqMethod == methodPost && length (pathInfo req) == 1 && game /= "history" && game `notElem` gameIDs then do
+        a <- atomically $ writeTVar tvar (game:gameIDs)
         respond response
     else respond response
 
 history :: [T.Text] -> Response
 history gameIDs = responseBuilder status200 [ ("Content-Type", "text/html") ] $ mconcat $ map copyByteString
-    [ "<h1>Game history</h1>", "<ul><li>", cs $ intercalate "<li>" $ map (++ "</li>") $ map T.unpack gameIDs, "</ul>"]
+    [ "<!DOCTYPE html>\n"
+    , "<html lang=\"en\">\n"
+    , "<head><title>Tic Tac Game History</title></head>\n"
+    , "<body>\n"
+    , "<h1>Game history</h1>\n" 
+    , "<ul>\n<li>"
+    , cs $ intercalate "\n<li>" $ map ((++ "</li>") . T.unpack) gameIDs, "</ul>"
+    , "\n</body>"]
 
 invalidRequest :: Response
 invalidRequest = responseBuilder status400 [ ("Content-Type", "text/plain") ] $ mconcat $ map copyByteString
@@ -60,21 +64,3 @@ postResponse body = do
             | finished = "---Game has ended---"
             | otherwise = responseBody
     responseBuilder status200 [ ("Content-Type", "application/bencode") ] $ mconcat $ map copyByteString [cs responseBody2]
-    
--- postResponse :: Request -> Response
--- postResponse req = do
---         let reqMethod = requestMethod req
---         let reqStr
---                 | reqMethod == methodPost = getResponseMove (requestBody req)
---                 | reqMethod == methodGet = return ("Invalid request method: Expected: POST, actual: GET")
---                 | otherwise = return ("?")
---         -- message <- reqStr
---         responseBuilder status200 [ ("Content-Type", "application/bencode") ] $ mconcat $ map copyByteString ["message"]
-
-getResponseMove :: B.ByteString -> B.ByteString
-getResponseMove body = "d1e1e"
-
-
-index x = responseBuilder status200 [("Content-Type", "text/html")] $ mconcat $ map copyByteString
-    [ "<p>Hello from ", BU.fromString $ show x, "!</p>"
-    , "<p><a href='/yay'>yay</a></p>\n" ]
